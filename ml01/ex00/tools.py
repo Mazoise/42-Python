49aa2535e44424ecf4f9c38059d012a9d8cb9786
mchardin <mchardin@student.42.fr>
2022-06-18 21:20:54 +0200

    ex00 done, starting ex01

---
+++ ml01/ex00/tools.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+
+
+def add_intercept(x):
+    """Adds a column of 1’s to the non-empty numpy.array x.
+    Args:
+        x: has to be an numpy.array, a vector or matrix.
+    Returns:
+        x as a numpy.array, a vector of shape m * 2.
+        None if x is not a numpy.array.
+        None if x is a empty numpy.array.
+    Raises:
+        This function should not raise any Exception.
+    """
+    if (type(x) != np.ndarray or len(x) == 0
+       or len(x.shape) != 2):
+        return None
+    try:
+        return np.insert(x, 0, 1, axis=1).astype(float)
+    except Exception:
+        return None
---
+++ ml01/ex01/test.py
@@ -0,0 +1,18 @@
+import numpy as np
+from vec_gradient import simple_gradient
+
+
+print("x = np.array([[12.4956442],[ 21.5007972],[ 31.5527382],\
+[ 48.9145838],[ 57.5088733]])")
+x = np.array([[12.4956442],[ 21.5007972],[ 31.5527382],
+[ 48.9145838],[ 57.5088733]])
+print("y = np.array([[37.4013816],[ 36.1473236],[ 45.7655287],\
+[ 46.6793434],[ 59.5585554]])")
+y = np.array([[37.4013816],[ 36.1473236],[ 45.7655287],
+[ 46.6793434],[ 59.5585554]])
+theta1 = np.array([[2],[ 0.7]])
+print("Test : theta 2, 0.7")
+print(simple_gradient(x, y, theta1))
+theta2 = np.array([[1],[ -0.4]])
+print("Test : theta 1, -0.4")
+print(simple_gradient(x, y, theta2))
